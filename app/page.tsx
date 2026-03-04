1a9e7680b8dfd8db91fe2e3ac61dff6d1e6dc301
Alex <alex@192.168.1.200>
2026-03-04 15:07:44 +0200

    Fix Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import BusMap from "./Map";
+import dynamic from "next/dynamic";
+
+const BusMap = dynamic(() => import("./Map"), { ssr: false });
 
 export default function Home() {
     return <BusMap />;
